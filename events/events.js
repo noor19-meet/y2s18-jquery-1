7a18873ed1c42bcc500ab878be179192ddc08336
aboud20-meet <aboud20@meet.mit.edu>
2018-08-02 18:10:11 +0300

    finished click me

--- events/events.js
+++ events/events.js
@@ -10,3 +10,8 @@ function randomY() {
 /* END PROVIDED CODE */
 
 // Write your code below this line :)
+$("#moveButton").on("click", function() {
+	$("#message").css("left", randomX());
+	$("#message").css("top", randomY());
+	
+});
